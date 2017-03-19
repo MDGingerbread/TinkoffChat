d16f17da777d0c8ccb79687288c65e21335f53c7
Maxim Danko <moscowdarkside@gmail.com>
2017-03-19 21:32:56 +0300

    Tinkoff Lesson 2

--- Tinkoff Chat/ViewController.swift
+++ Tinkoff Chat/ViewController.swift
@@ -8,18 +8,210 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
 
+    
+    var isChangeProfileImageView = false
+    
+    let imagePicker = UIImagePickerController()
+    
+    @IBOutlet weak var userName: UITextField!
+    
+    @IBOutlet weak var colorText: UILabel!
+    
+    @IBOutlet weak var userDescription: UITextView!
+   
+    @IBOutlet weak var userProfileImageView: UIImageView!
+    
+    @IBAction func changeColorAction(_ sender: Any) {
+        
+        guard let button = sender as? UIButton else {
+            return
+        }
+        
+        colorText.textColor = button.backgroundColor
+    }
+    
+    @IBAction func saveAction(_ sender: Any) {
+        
+        print("Сохранение данных профиля")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //*************
+        print(#function)
+        print(userName.description)
+        print(userDescription.description)
+        print(userProfileImageView.description)
+        print(colorText.description)
+        //*************
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        //*************
+        print(#function)
+        print(userName.description)
+        print(userDescription.description)
+        print(userProfileImageView.description)
+        print(colorText.description)
+        //*************
+
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        //*************
+        print(#function)
+        print(userName.description)
+        print(userDescription.description)
+        print(userProfileImageView.description)
+        print(colorText.description)
+        //*************
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        //*************
+        print(#function)
+        print(userName.description)
+        print(userDescription.description)
+        print(userProfileImageView.description)
+        print(colorText.description)
+        //*************
+    }
+    
+    deinit {
+        //*************
+        print(#function)
+        print(userName.description)
+        print(userDescription.description)
+        print(userProfileImageView.description)
+        print(colorText.description)
+        //*************
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //*************
+        print(#function)
+        print(userName.description)
+        print(userDescription.description)
+        print(userProfileImageView.description)
+        print(colorText.description)
+        //*************
+        userName.delegate = self
+        
+        userName.returnKeyType = .done
+        
+        userDescription.delegate = self
+        
+        userDescription.returnKeyType = .done
+        
+        userProfileImageView.isUserInteractionEnabled = true
+        
+        let chooseImage = UITapGestureRecognizer(target: self, action: #selector(self.chooseImage))
+        
+        userProfileImageView.addGestureRecognizer(chooseImage)
+        
+        imagePicker.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if (text == "\n") {
+            textView.resignFirstResponder()
+            return false
+        }
+        return true
+    }
+    
+    func chooseFromLibrary() {
+        
+        imagePicker.allowsEditing = false
+        
+        imagePicker.sourceType = .photoLibrary
+        
+        imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+        
+        imagePicker.allowsEditing = true
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func takePhoto() {
+        
+        imagePicker.allowsEditing = false
+        
+        imagePicker.sourceType = .camera
+        
+        imagePicker.cameraCaptureMode = .photo
+        
+        imagePicker.modalPresentationStyle = .fullScreen
+        
+        imagePicker.allowsEditing = true
 
+        
+        present(imagePicker,animated: true,completion: nil)
+    }
+    
+    func chooseImage() {
+        let choseAlertController = UIAlertController(title: "", message: "", preferredStyle: .actionSheet)
+        
+        let actionChooseFromLibrary = UIAlertAction(title: "Выбрать из галлереи", style: .default, handler: { action in
+            self.chooseFromLibrary()
+        })
+        
+        choseAlertController.addAction(actionChooseFromLibrary)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let actionTakePhoto = UIAlertAction(title: "Сделать фото", style: .default, handler: {action in
+                self.takePhoto()
+            })
+            choseAlertController.addAction(actionTakePhoto)
+        }
+        
+        if isChangeProfileImageView {
+            let actionDeletePhoto = UIAlertAction(title: "Удалить фото", style: .default, handler: {action in
+                self.userProfileImageView.image = UIImage(named: "defaultProfileImageView")
+                self.isChangeProfileImageView = false
+            })
+            choseAlertController.addAction(actionDeletePhoto)
+        }
+        
+        
+        let actionCancel = UIAlertAction(title: "Отмена", style: .cancel, handler: nil)
+        
+        choseAlertController.addAction(actionCancel)
+        
+        present(choseAlertController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        var  chosenImage = UIImage()
+        
+        chosenImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        
+        userProfileImageView.contentMode = .scaleAspectFit
+        
+        userProfileImageView.image = chosenImage
+        
+        isChangeProfileImageView = true
+        
+        dismiss(animated:true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
 
 }
 
